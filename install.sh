d41722ca3112462dbd8c4611459cc6c5d56463aa
Ecole de la liberté <74274427+Ecoledelaliberte@users.noreply.github.com>
2021-05-20 12:12:32 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo -e "Installation de TanspoFilms \n" 
+echo -e "Installation de git: \n"
+sudo apt install git-all
+echo -e "Téléchargement de TranspoFilms: \n"
+git clone https://github.com/Ecoledelaliberte/transport.git
+echo -e "Téléchargement des Films: \n"
+
